f9b780a7c698b2ca7f2e01f6fc304a2cc4ef1c8d
Siripong Chusri <siripong.chu@ku.th>
2020-04-11 03:42:11 +0700

    first commit

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const functions = require('firebase-functions');
+const request = require('request-promise');
+const admin = require('firebase-admin');
+const region = 'asia-east2';
+const runtimeOpts = {
+  timeoutSeconds: 4,
+  memory: "2GB"
+};
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
+
+const LINE_HEADER = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer Pp6phRvvrfzI5GGPZ+ByKEq/ypv5edDTfh8du1Ij0SCl9if21h0VyCcGRwurc6bCjCshMnMqZ2F+oxfSfiSXKHpDewSloJZloS8WOjhfgnctfwvc/nDLiJc/RED3FXj/ufaL/L84qllM51lv3ZcBewdB04t89/1O/w1cDnyilFU=" 
+};
+
+admin.initializeApp();
+
+exports.Test_Chatbot = functions.region(region).runWith(runtimeOpts).https.onRequest((req, res) => {
+ if (req.method === "POST") {
+    let event = req.body.events[0]
+    if (event.type === "message" && event.message.type === "text") {
+      postToDialogflow(req);
+    } else {
+      reply(req);
+    }
+  }
+  return res.status(200).send(req.method);
+});
+
+const reply = req => {
+    return request.post({
+      uri: `${LINE_MESSAGING_API}/reply`,
+      headers: LINE_HEADER,
+      body: JSON.stringify({
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+          {
+            type: "text",
+            text: JSON.stringify(req.body)
+          }
+        ]
+      })
+    });
+  };
+  
+  
+  const postToDialogflow = req => {
+    req.headers.host = "bots.dialogflow.com";
+    return request.post({
+      uri: "https://bots.dialogflow.com/line/2507c003-79b0-4ae8-ab7b-e02a15b246b1/webhook",
+      headers: req.headers,
+      body: JSON.stringify(req.body)
+    });
+  };
