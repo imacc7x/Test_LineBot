0646a12892a189bd9443689b1a2a1dc07bc64ac6
Siripong Chusri <siripong.chu@ku.th>
2020-05-04 00:28:45 +0700

    dialogflow

---
+++ functions/dialogflow.js
@@ -0,0 +1,114 @@
+// test
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https//test-chatbot-uyotlh.firebaseio.com"
+});
+const db = admin.firestore();
+const region = 'asia-east2';
+const runtimeOpts = {
+  timeoutSeconds: 4,
+  memory: "2GB"
+};
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+exports.dialogflowFirebaseFulfillment = functions.region(region).runWith(runtimeOpts).https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function activation(agent) {
+    agent.add('ขอบคุณมากค่ะ ดิฉันมั่นใจว่าข้อมูลที่คุณให้จะเป็นประโยชน์แก่ทีมผู้สรัางดิฉัน ในการพัฒนาการดูแลผู้ดื่มเหล้าต่อไปแน่นอนค่ะ');
+    agent.add('ข้อมูลเบื้องต้นที่ดิฉันจำเป็นต้องทราบ คุณอายุเท่าไหร่คะ');
+  }
+
+  function setAge(agent) {
+    console.log ("This is setage function");
+    const userId = request.body.originalDetectIntentRequest.payload.data.source.userId;
+    agent.add("Your userID: " + userId);
+    let age = agent.parameters.age;
+    agent.add("Your age: " + age);
+	db.collection("Users").doc(userId).update({
+      	age: age
+    });
+    agent.add("คุณทำงานอะไรเป็นอาชีพหลักคะ");
+  }
+  
+
+  function test(agent) {
+    agent.add('success');
+    //agent.add(JSON.stringify(request.body.originalDetectIntentRequest.payload.data.source.userId));
+    const userId = request.body.originalDetectIntentRequest.payload.data.source.userId;
+    agent.add('userId ' + userId);
+    console.log("console log ",  userId);
+    const users = db.collection("Users");
+   	
+    return users.doc(userId).get()
+      .then(doc => {
+        if (!doc.exists) {
+          agent.add("Not Found");
+        } else {
+          agent.add("This is User ID: " + doc.data().name);
+        }
+      });
+  }
+ 
+
+
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('Activation-yes', activation);
+  intentMap.set('Set Age', setAge);
+  intentMap.set('test', test);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
