06471619cf96c486ed95a76f15ab656a9b172a59
chalermchon <chalermchon.o@ku.th>
2020-05-05 03:58:27 +0700

    Fix conflic

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const request = require('request-promise');
 
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
@@ -16,14 +16,13 @@ exports.handler = (req, res, db) => {
         const docUser = db.collection("Users").doc(source.userId);
 
         if (type === "follow") {
-            console.log("LineHeader: ", LINE_HEADER);
             follow(docUser, event.replyToken)
-                .then( () => res.status(200).send("Follow is ok.") )
+                .then(() => res.status(200).send("Follow is ok."))
                 .catch((err) => console.error("Follow Error: ", err))
         }
         else if (type === "unfollow") {
-            docUser.update({ active: false })
-                .then( () => res.status(200).send("Unfollow is ok.") )
+            unfollow(docUser, source.userId)
+                .then(() => res.status(200).send("Unfollow is ok."))
                 .catch((err) => console.error("Unfollow Error: ", err))
         }
         else if (type === "message") {
@@ -48,7 +47,6 @@ const reply = (replyToken, messages) => {
         })
     });
 };
-
 const push = (userId, messages) => {
     return request.post({
         uri: `${LINE_MESSAGING_API}/push`,
@@ -60,76 +58,68 @@ const push = (userId, messages) => {
     });
 };
 
-const follow = (documentUser, userId) => {
-    return documentUser.get()
-        .then(docSnapshot => {
-            if (!docSnapshot.exists) {
-                return documentUser.set({
-                    active: true
-                });
-            }
-            else {
-                return documentUser.update({
-                    active: true
-                });
-            }
-        })
-        .then(() => {
-            return push(
-                userId,
-                [
-                    {
-                        type: "text",
-                        text: "สวัสดีค่ะ ดิฉันเป็นบอทผู้ช่วยนักให้คำปรึกษาของศูนย์เลิกเหล้า 1413 ยินดีที่ได้พูดคุยกับคุณในวันนี้ค่ะ"
-                    },
-                    {
-                        type: "text",
-                        text: "ฉันสามารถให้ข้อมูลเบื้องต้นเกี่ยวกับการดื่มแก่คุณได้ตลอด 24 ชั่วโมง แม้ว่าบางคำถามของคุณ ดิฉันอาจไม่สามารถเข้าใจได้"
-                    },
-                    {
-                        type: "text",
-                        text: "แต่ดิฉันก็จะช่วยสรุปข้อมูลที่สำคัญทั้งหมดและส่งต่อให้แก่นักให้คำปรึกษาค่ะดิฉันมั่นใจว่านักให้คำปรึกษาจะช่วยคุณได้แน่นอน"
-                    },
-                    {
-                        type: "text",
-                        text: "โดยข้อมูลที่ได้จากการสนทนาที่จะสามารถระบุตัวตนของคุณได้จะไม่มีการเผยแพร่ ดิฉันจึงอยากขอให้คุณอนุญาตให้พวกเขาทำเช่นนั้นก่อน"
-                    },
-                    {
-                        type: "text",
-                        text: "คุณจะอนุญาตได้ไหมคะ",
-                        quickReply: {
-                            items: [
-                                {
-                                    type: "action",
-                                    action: {
-                                        type: "postback",
-                                        label: "อนุญาติ",
-                                        data: "ACTIVATING_CONFIRM"
-                                    }
-                                },
-                                {
-                                    type: "action",
-                                    action: {
-                                        type: "postback",
-                                        label: "ไม่อนุญาติ",
-                                        data: "ACTIVATING_NOT_CONFIRM"
-                                    }
-                                }
-                            ]
+const follow = async (documentUser, replyToken) => {
+    const user = await documentUser.get()
+    if (!user.exists) {
+        await documentUser.set({ active: true });
+    }
+    else {
+        await documentUser.update({ active: true });
+    }
+    reply(
+        replyToken,
+        [
+            {
+                type: "text",
+                text: "สวัสดีค่ะ ดิฉันเป็นบอทผู้ช่วยนักให้คำปรึกษาของศูนย์เลิกเหล้า 1413 ยินดีที่ได้พูดคุยกับคุณในวันนี้ค่ะ"
+            },
+            {
+                type: "text",
+                text: "ฉันสามารถให้ข้อมูลเบื้องต้นเกี่ยวกับการดื่มแก่คุณได้ตลอด 24 ชั่วโมง แม้ว่าบางคำถามของคุณ ดิฉันอาจไม่สามารถเข้าใจได้"
+            },
+            {
+                type: "text",
+                text: "แต่ดิฉันก็จะช่วยสรุปข้อมูลที่สำคัญทั้งหมดและส่งต่อให้แก่นักให้คำปรึกษาค่ะดิฉันมั่นใจว่านักให้คำปรึกษาจะช่วยคุณได้แน่นอน"
+            },
+            {
+                type: "text",
+                text: "โดยข้อมูลที่ได้จากการสนทนาที่จะสามารถระบุตัวตนของคุณได้จะไม่มีการเผยแพร่ ดิฉันจึงอยากขอให้คุณอนุญาตให้พวกเขาทำเช่นนั้นก่อน"
+            },
+            {
+                type: "text",
+                text: "คุณจะอนุญาตได้ไหมคะ",
+                quickReply: {
+                    items: [
+                        {
+                            type: "action",
+                            action: {
+                                type: "postback",
+                                label: "อนุญาติ",
+                                data: "ACTIVATING_CONFIRM"
+                            }
+                        },
+                        {
+                            type: "action",
+                            action: {
+                                type: "postback",
+                                label: "ไม่อนุญาติ",
+                                data: "ACTIVATING_NOT_CONFIRM"
+                            }
                         }
-                    }
-                ]
-            );
-        })
+                    ]
+                }
+            }
+        ]
+    );
+    return new Promise((resolve, reject) => resolve());
 };
-
-const unfollow = (documentUser, userId) => {
-    documentUser.update({
+const unfollow = async (documentUser, userId) => {
+    await documentUser.update({
         active: false
-    })
-        .then(() => console.log(userId + ": unfollow"))
-        .catch((err) => console.error("Unfollow error: ", err))
-}
+    });
+    console.log(userId + ": unfollow");
+    return new Promise((resolve, reject) => resolve());
+};
 
 const postToDialogflow = req => {
     req.headers.host = "bots.dialogflow.com";
