c86c99e9c89730b573a947c58a3210297e33a14d
Siripong Chusri <siripong.chu@ku.th>
2020-06-29 00:56:44 +0700

    test

--- functions/dialogflow.js
+++ functions/dialogflow.js
@@ -226,7 +226,7 @@ exports.handler = (request, response, firebaseAdmin) => {
                         drinkingPoint = 8;
                     }
 
-                    const result = 0.79 * percent * capacity
+                    const result = drinkingPoint/(0.79 * percent * capacity);
                     agent.add("result: " + result);
 
                     agent.add(
